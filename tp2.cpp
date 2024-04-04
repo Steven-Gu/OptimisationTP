633e9bca110338dd360c6cc156ad7c3e155ad5d5
hunter-zhang@outlook.com <hunter-zhang@outlook.com>
2024-04-04 17:57:15 +0200

    a

--- tp2.cpp
+++ tp2.cpp
@@ -1,50 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <utility> // Pour std::pair
+#include <algorithm> 
 
 using namespace std;
 
 struct Vertex {
-    int id; 
-    int k;
+    int begin;
+    int end;
+    int k; 
 
-    Vertex(int i){
-        id = i;
-        k = 0;
-    }
+    Vertex(int b, int e) : begin(b), end(e), k(0) {}
 };
-
 
 struct Graph {
     vector<Vertex> S;  
-    vector< pair<int,int> > A;  
 
-    void addVertex(Vertex vertex) {
+    void addVertex(const Vertex& vertex) {
         S.push_back(vertex);
     }
 
-    // Fonction pour ajouter une arête au graphe
-    void addEdge(int vertexId1, int vertexId2) {
-        A.emplace_back(vertexId1, vertexId2);
-    }
 
     void colorVertices() {
+        sort(S.begin(), S.end(), [](Vertex a,Vertex b) {
+            return a.begin < b.begin;
+        });
+
         for (auto& vertex : S) {
-            vector<bool> used(S.size() + 1, false); // Marqueur pour les couleurs utilisées
-            
-            // Parcourir toutes les arêtes pour marquer les couleurs utilisées par les voisins
-            for (const auto& edge : A) {
-                if (edge.first == vertex.id && S[edge.second - 1].k != 0) { // -1 car l'indexation commence à 0
-                    used[S[edge.second - 1].k] = true;
-                } else if (edge.second == vertex.id && S[edge.first - 1].k != 0) {
-                    used[S[edge.first - 1].k] = true;
+            vector<bool> used(S.size(), false);
+
+            for (const auto& other : S) {
+                if (&vertex != &other && !(vertex.end <= other.begin || other.end <= vertex.begin)) {
+                    if (other.k != 0) { 
+                        used[other.k - 1] = true; 
+                    }
                 }
             }
 
-            // Attribuer la plus petite couleur non utilisée
-            for (int i = 1; i <= S.size(); ++i) {
+            
+            for (int i = 0; i < used.size(); ++i) {
                 if (!used[i]) {
-                    vertex.k = i;
+                    vertex.k = i + 1;
                     break;
                 }
             }
@@ -52,39 +47,25 @@ struct Graph {
     }
 };
 
-
-
-
 int main() {
     Graph g;
 
-    // Ajout de sommets au graphe
-    g.addVertex(Vertex(1)); 
-    g.addVertex(Vertex(2)); 
-    g.addVertex(Vertex(3)); 
-    g.addVertex(Vertex(4));
-    g.addVertex(Vertex(5));
-    g.addVertex(Vertex(6));
-    g.addVertex(Vertex(7)); 
+    
+    g.addVertex(Vertex(1, 3));
+    g.addVertex(Vertex(2, 5));
+    g.addVertex(Vertex(2, 8));
+    g.addVertex(Vertex(4, 6));
+    g.addVertex(Vertex(6, 11));
+    g.addVertex(Vertex(9, 11));
+    g.addVertex(Vertex(10, 12));
 
-
-    // Ajout d'arêtes au graphe
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.addEdge(2, 4);
-    g.addEdge(3, 5);
-    g.addEdge(5, 6);
-    g.addEdge(5, 7);
-    g.addEdge(6, 7);
-
+   
     g.colorVertices();
 
-    // Affichage des couleurs des sommets
-    cout << "Couleurs des sommets:" << endl;
+    
+    cout << "Vertex colors:" << endl;
     for (const auto& vertex : g.S) {
-        cout << "Sommet " << vertex.id << " a la couleur " << vertex.k << endl;
+        cout << "Vertex [" << vertex.begin << ", " << vertex.end << "] has color " << vertex.k << endl;
     }
 
     return 0;
